35cb1aeebef4a15b600b26ed9dd699494e90d624
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-25 01:20:11 +0100

    Initial commit

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QRFeedz\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
