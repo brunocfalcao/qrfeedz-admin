18219058ba51b07849c7b50d3566ed41feb61253
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-13 18:32:32 +0100

    wip

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -2,9 +2,21 @@
 
 namespace QRFeedz\Admin;
 
-use Laravel\Nova\Nova;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+use QRFeedz\Admin\Resources\Authorization;
+use QRFeedz\Admin\Resources\Category;
+use QRFeedz\Admin\Resources\Client;
+use QRFeedz\Admin\Resources\Country;
+use QRFeedz\Admin\Resources\Group;
+use QRFeedz\Admin\Resources\Locale;
+use QRFeedz\Admin\Resources\Question;
+use QRFeedz\Admin\Resources\Questionnaire;
+use QRFeedz\Admin\Resources\Response;
+use QRFeedz\Admin\Resources\Tag;
 use QRFeedz\Admin\Resources\User;
+use QRFeedz\Admin\Resources\UserAuthorization;
+use QRFeedz\Admin\Resources\Widget;
 
 class AdminServiceProvider extends ServiceProvider
 {
@@ -12,6 +24,18 @@ class AdminServiceProvider extends ServiceProvider
     {
         Nova::resources([
             User::class,
+            Country::class,
+            Authorization::class,
+            Category::class,
+            Client::class,
+            Group::class,
+            Locale::class,
+            Question::class,
+            Questionnaire::class,
+            Response::class,
+            Tag::class,
+            UserAuthorization::class,
+            Widget::class
         ]);
     }
 
--- src/Resources/User.php
+++ src/Resources/User.php
@@ -5,38 +5,33 @@ namespace QRFeedz\Admin\Resources;
 use App\Nova\Resource;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
-use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\DateTime;
-use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use QRFeedz\Admin\Actions\ResetPassword;
 
 class User extends Resource
 {
-    /**
-     * The model the resource corresponds to.
-     *
-     * @var class-string<\App\User>
-     */
     public static $model = \QRFeedz\Cube\Models\User::class;
 
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'name';
-
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
     public static $search = [
-        'id', 'name', 'email',
+        'id', 'name', 'email', 'phone_number'
     ];
+
+    public function title()
+    {
+        return $this->name;
+    }
+
+    public function subtitle()
+    {
+        if ($this->client) {
+            return $this->client->name;
+        }
+    }
 
     public static function indexQuery(NovaRequest $request, $query)
     {
@@ -54,8 +49,11 @@ class User extends Resource
             return $query;
         }
 
-        // Admin for clients? -- Can see all users from its client.
-        if ($user->isAuthorized('admin', ['client_id' => $user->client_id])) {
+        /**
+         * Only users that have "admin" authorization on the respective
+         * client, can manage users from its own client.
+         */
+        if ($user->isAuthorizedAs($user->client, 'admin')) {
             return $query->where('client_id', $user->client_id);
         }
 
@@ -83,14 +81,37 @@ class User extends Resource
                 ->creationRules('unique:users,email')
                 ->updateRules('unique:users,email,{{resourceId}}'),
 
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', Rules\Password::defaults())
-                ->updateRules('nullable', Rules\Password::defaults()),
+            Text::make('Phone Number'),
 
-            DateTime::make('Created At')->readonly(),
-            DateTime::make('Updated At')->readonly(),
-            DateTime::make('Deleted At')->readonly(),
+            Boolean::make('Is super admin?', 'is_admin')
+                ->canSee(function ($request) {
+                    return $request->user()->is_admin;
+                }),
+
+            DateTime::make('Created At')
+                    ->hideFromIndex()
+                    ->readonly()
+                    ->displayUsing(function ($value) {
+                        return $value->diffForHumans();
+                    }),
+
+            DateTime::make('Updated At')
+                    ->hideFromIndex()
+                    ->readonly()
+                    ->displayUsing(function ($value) {
+                        return $value->diffForHumans();
+                    }),
+
+            DateTime::make('Deleted At')
+                ->hideFromIndex()
+                ->readonly()
+                ->canSee(function ($request) {
+                    $model = $request->findModel();
+                    return !$model->deleted_at == null;
+                })
+                ->displayUsing(function ($value) {
+                    return $value?->diffForHumans();
+                })
         ];
     }
 
@@ -131,6 +152,30 @@ class User extends Resource
      */
     public function actions(NovaRequest $request)
     {
-        return [];
+        return [
+            (new ResetPassword())
+                ->onlyOnDetail()
+                ->size('5xl')
+                ->canSee(function () use ($request) {
+
+                    return true;
+
+                    // Get targeted resource.
+                    $model = $request->findModel();
+
+                    // Get logged user.
+                    $user = $request->user();
+
+                    return
+                        // Is super admin.
+                        $request->user()->is_admin ||
+
+                        // Is the same user.
+                        $user->id == $model->id ||
+
+                        // Is a client-based admin.
+                        $user->isAuthorizedAs($user->client, 'admin');
+                })
+        ];
     }
 }
