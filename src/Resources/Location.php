c507df138d768a409a3183edd962c8e9ba111ed2
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-30 14:53:18 +0200

    Update Location.php

--- src/Resources/Location.php
+++ src/Resources/Location.php
@@ -11,8 +11,15 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
 use QRFeedz\Admin\Fields\FKLink;
 use QRFeedz\Admin\Fields\IDSuperAdmin;
+use QRFeedz\Admin\Resources\Country as CountryResource;
 use QRFeedz\Admin\Traits\DefaultDescPKSorting;
+use QRFeedz\Cube\Models\Country;
 use QRFeedz\Foundation\Abstracts\QRFeedzResource;
+use Trinityrank\GoogleMapWithAutocomplete\TRAddress;
+use Trinityrank\GoogleMapWithAutocomplete\TRCity;
+use Trinityrank\GoogleMapWithAutocomplete\TRCountry;
+use Trinityrank\GoogleMapWithAutocomplete\TRMap;
+use Trinityrank\GoogleMapWithAutocomplete\TRZipCode;
 
 class Location extends QRFeedzResource
 {
@@ -23,7 +30,7 @@ class Location extends QRFeedzResource
     public static $title = 'name';
 
     public static $search = [
-        'name', 'address', 'postal_code', 'locality',
+        'name', 'address', 'postal_code', 'city',
     ];
 
     public function subtitle()
@@ -55,17 +62,33 @@ class Location extends QRFeedzResource
             Text::make('Name')
                 ->rules('required', 'max:255'),
 
-            Text::make('Address')
-                ->rules('required', 'max:255'),
+            TRAddress::make('Address')
+                     ->rules('required'),
 
-            Text::make('Postal Code')
-                ->rules('required', 'max:255'),
+            TRZipCode::make('Zip Code', 'postal_code')
+                     ->hideFromIndex(),
 
-            Text::make('Locality')
-                ->rules('required', 'max:255'),
+            TRCity::make('City')
+                  ->hideFromIndex(),
 
-            BelongsTo::make('Country', 'country')
-                     ->withoutTrashed(),
+            BelongsTo::make('Country', 'country', CountryResource::class)
+                     ->readonlyIfViaResource()
+                     ->exceptOnForms(),
+
+            TRCountry::make('Country', 'country_id')
+                     ->resolveUsing(function ($value) {
+                         return Country::firstWhere('id', $value)?->name;
+                     })
+                     ->fillUsing(function ($request, $model, $attribute, $requestAttribute) {
+                         $model->{$attribute} = Country::firstWhere('name', $request->input($attribute))->id;
+                     })
+                     ->onlyOnForms()
+                     ->rules('required'),
+
+            TRMap::make('Map')
+                 ->hideLatitude()
+                 ->hideLongitude()
+                 ->onlyOnForms(),
 
             new Panel('Timestamps', $this->timestamps($request)),
 
